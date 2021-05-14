06b93a3cedf5f219cafc57245e6c0e163f06af3d
LAZ-R <crazydrawer62@gmail.com>
2021-05-14 09:37:57 +0200

    Ajout de la JavaDoc sur la classe RendezVous, et formatage de l'affichage console sur la méthode afficher() de la classe Rendez-vous.

--- src/fr/eni/ecole/quelMedecin/bo/RendezVous.java
+++ src/fr/eni/ecole/quelMedecin/bo/RendezVous.java
@@ -5,6 +5,11 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.Date;
 
+/** Classe qui représente un Rendez-vous.
+ *
+ * @author laz_R
+ * @version 1.0
+ */
 public class RendezVous {
 
     // Attributs d'instance
@@ -12,12 +17,16 @@ public class RendezVous {
     private Patient patient;
     private LocalDate date;
 
-
-
     // Attributs de classe
 
     // Méthodes
 
+    /** Constructeur avec tous les paramètres.
+     *
+     * @param creneau Creneau
+     * @param patient Patient
+     * @param date LocalDate
+     */
     public RendezVous(Creneau creneau, Patient patient, LocalDate date) {
 
         this.creneau = creneau;
@@ -25,10 +34,15 @@ public class RendezVous {
         this.date = date;
     }
 
+    /** Méthode d'affichage du rendez-vous.<br>
+     *  Récupère le médecin associé à ce créneau pour l'afficher.<br>
+     *  Affiche également les informations du patient associé à ce rendez-vous.
+     */
     public void afficher() {
-        System.out.println(this.date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG)));
+        System.out.printf("Rendez-vous du %s / ", this.date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG)));
         this.creneau.afficher();
-        System.out.println(this.creneau.getMedecin().getNomComplet());
+        System.out.printf("avec le Dr. %s%n", this.creneau.getMedecin().getNomComplet());
+        System.out.printf("pour ");
         this.patient.afficher();
     }
 }
